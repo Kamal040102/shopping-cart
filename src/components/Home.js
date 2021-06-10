adf8ef89623ca11193dfa96d719aabca5f6c438b
Kamal A Sharma <kamal5201ks@gmail.com>
2021-06-11 01:24:00 +0530

    Initial Commit

---
+++ src/components/Home.js
@@ -0,0 +1,53 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux'
+import { addToCart } from './actions/cartActions'
+
+ class Home extends Component{
+
+    handleClick = (id)=>{
+        this.props.addToCart(id); 
+    }
+
+    render(){
+        let itemList = this.props.items.map(item=>{
+            return(
+                <div className="card" style={{margin:"20px", width:"20rem", height:"auto"}} key={item.id}>
+                        <div className="card-image">
+                            <img src={item.img} alt={item.title} style={{height:"400px"}}/>
+                            <span to="/" className="btn-floating halfway-fab waves-effect waves-light red"><i className="material-icons" onClick= {()=>{this.handleClick(item.id)}}>add</i></span>
+                        </div>
+
+                        <div className="card-content">
+                            <span className="card-title">{item.title}</span>
+                            <p>{item.desc}</p>
+                        </div>
+                        <div className="card-content" style={{paddingTop:"0"}}>
+                        <p><b>Price: ₹ {item.price}</b></p>
+                        </div>
+                 </div>
+            )
+        })
+        return(
+            <div className="container">
+                <h3 className="center">Populars</h3>
+                <div className="box" style={{display:"flex", flexWrap:"wrap"}}>
+                    {itemList}
+                </div>
+            </div>
+        )
+    }
+}
+const mapStateToProps = (state)=>{
+    return {
+      items: state.items
+    }
+  }
+
+  const mapDispatchToProps= (dispatch)=>{
+    
+    return{
+        addToCart: (id)=>{dispatch(addToCart(id))}
+    }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Home)
---
+++ src/components/actions/cartActions.js
@@ -0,0 +1,39 @@
+import {ADD_TO_CART} from './action-types/cart-action'
+import {REMOVE_ITEM} from './action-types/cart-action'
+import {SUB_QUANTITY} from './action-types/cart-action'
+import {ADD_QUANTITY} from './action-types/cart-action'
+import {ADD_SHIPPING} from './action-types/cart-action'
+
+export const addToCart = (id) => {
+    return {
+        type: ADD_TO_CART,
+        id
+    }
+}
+
+export const removeItem = (id) => {
+    return{
+        type: REMOVE_ITEM,
+        id
+    }
+}
+export const subtractQuantity = (id) =>{
+    return {
+        type: SUB_QUANTITY,
+        id
+    }
+}
+
+export const addQuantity = (id) =>{
+    return{
+        type: ADD_QUANTITY,
+        id
+    }
+}
+
+export const addShipping = (id) => {
+    return{
+        type: ADD_SHIPPING,
+        id
+    }
+}
